55d4e6b50a6c1f511f9ffa2eda17565eb712dae3
Luis Osnet <osnet@outlook.com>
2017-09-20 00:46:28 -0500

    agrego modelo edificio y migracion

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170920054345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "edificios", force: :cascade do |t|
+    t.string   "direccion"
+    t.string   "colonia"
+    t.string   "estado"
+    t.string   "link"
+    t.text     "notas"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
