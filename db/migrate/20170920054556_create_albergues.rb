b6b8a772bd90101afc4c84caa7b13d0d1e7b2828
Luis Osnet <osnet@outlook.com>
2017-09-20 03:44:35 -0500

    Agrego cambion en los tipos de datos

--- db/migrate/20170920054556_create_albergues.rb
+++ db/migrate/20170920054556_create_albergues.rb
@@ -1,7 +1,7 @@
 class CreateAlbergues < ActiveRecord::Migration[5.0]
   def change
     create_table :albergues do |t|
-      t.integer :tipo
+      t.string :nombre
       t.text :direccion
       t.string :url
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170920080042) do
+ActiveRecord::Schema.define(version: 20170920083440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "albergues", force: :cascade do |t|
-    t.integer  "tipo"
+    t.string   "nombre"
     t.text     "direccion"
     t.string   "url"
     t.datetime "created_at", null: false
