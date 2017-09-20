93057a88af0194d51f98daf168310836b4f64795
Angel Buzany <angelbuzany@gmail.com>
2017-09-20 00:48:16 -0500

    Agrego modelo y migraciones de albergues

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170920053608) do
+ActiveRecord::Schema.define(version: 20170920054556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "albergues", force: :cascade do |t|
+    t.integer  "tipo"
+    t.text     "direccion"
+    t.string   "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "personas", force: :cascade do |t|
     t.string   "nombre"
