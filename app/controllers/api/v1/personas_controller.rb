cad0c3eb367eaf83e56400482298b202b97873e6
Luis Osnet <osnet@outlook.com>
2017-09-20 01:38:35 -0500

    quito pry

--- app/controllers/api/v1/personas_controller.rb
+++ app/controllers/api/v1/personas_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::PersonasController < Api::V1::BaseController
   before_action :busca_personas, only: [:index]
 
   def index
-    binding.pry
     response_success personas: @personas
   end
 
